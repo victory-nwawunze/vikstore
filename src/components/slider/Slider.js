03c447f66d2fb33bdcff3fdb03c697a1875a8d98
victory-nwawunze <victorynwawunze@gmail.com>
2023-08-09 19:41:29 +0100

    forth-commit

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -3,6 +3,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Slider.css";
+import Img1 from "../../images/pexels-anna-shvets-3962294.jpg";
+import Img2 from "../../images/pexels-gustavo-fring-3985062.jpg";
+import Img3 from "../../images/pexels-sam-lion-5709656.jpg";
 export default class SimpleSlider extends Component {
   render() {
     const settings = {
@@ -17,14 +20,26 @@ export default class SimpleSlider extends Component {
     return (
       <div className="slider-container">
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+          <div className="slide-1">
+            <h1 className="heading-text">Lorem ipsum dolor sit.</h1>
+            <p className="description-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <button className="btn">Login</button>
           </div>
-          <div>
-            <h3>2</h3>
+          <div className="slide-2">
+            <h1 className="heading-text">Lorem ipsum dolor sit.</h1>
+            <p className="description-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <button className="btn">Login</button>
           </div>
-          <div>
-            <h3>3</h3>
+          <div className="slide-3">
+            <h1 className="heading-text">Lorem ipsum dolor sit.</h1>
+            <p className="description-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <button className="btn">Login</button>
           </div>
         </Slider>
       </div>
