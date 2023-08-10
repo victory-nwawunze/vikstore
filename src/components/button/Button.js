4f915cc65054e6c14279b851fb3e7444f3e92679
victory-nwawunze <victorynwawunze@gmail.com>
2023-08-10 08:58:05 +0100

    sixth-commit

---
+++ src/components/button/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Button = ({ children, style, onClick }) => {
+  return (
+    <button style={style} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+export default Button;
