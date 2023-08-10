6e7ff9bd965eb3e5a07dd8e43405f257ef06b6e3
victory-nwawunze <victorynwawunze@gmail.com>
2023-08-10 14:32:45 +0100

    sixth-commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Favourites from "./components/favourite/Favourites";
 import Navbar from "./components/navbar/Navbar";
+import Products from "./components/products/Products";
 import Slider from "./components/slider/Slider";
 import TrendingProducts from "./components/trending/TrendingProducts";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar />
       <Slider />
       <Favourites />
-      <TrendingProducts />
+      <TrendingProducts /> */}
+      <Products />
     </div>
   );
 };
